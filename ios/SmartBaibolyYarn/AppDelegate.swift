618f6e59652f85e6836f950a70daf657a2372dbf
Arotiana137-51 <arotiana4612@gmail.com>
2026-03-22 20:06:42 +0300

    App rename

--- ios/SmartBaibolyYarn/AppDelegate.swift
+++ ios/SmartBaibolyYarn/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "SmartBaibolyYarn",
+      withModuleName: "eBaiboly",
       in: window,
       launchOptions: launchOptions
     )
